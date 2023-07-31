c445a8a7291d89437bc06671953ad14477bb7608
Danila <icy.calmness@gmail.com>
2023-07-31 19:03:39 +0300

    create tables in DATABASE north;

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,36 @@
 -- SQL-команды для создания таблиц
+
+-- CREATE TABLE employees
+CREATE TABLE employees
+(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128) NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    birth_date DATE NOT NULL,
+    notes TEXT
+);
+
+
+
+-- CREATE TABLE customers
+CREATE TABLE customers
+(
+    customer_id VARCHAR(128) PRIMARY KEY,
+    company_name VARCHAR(128) NOT NULL,
+    contact_name VARCHAR(128) NOT NULL
+);
+
+
+
+-- CREATE TABLE orders
+CREATE TABLE orders
+(
+    order_id SERIAL PRIMARY KEY,
+    customer_id VARCHAR(128) REFERENCES customers(customer_id) ON DELETE CASCADE,
+    employee_id INTEGER REFERENCES employees(employee_id) ON DELETE CASCADE,
+    order_date DATE NOT NULL,
+    ship_city VARCHAR(128) NOT NULL
+    -- FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    -- FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
